b8f996469ce89d7c47e50f2111c1266fc1f4f754
Kristina Zabunova <triara.xiii@gmail.com>
2015-08-28 18:00:56 -0700

    refactor: removed GET /api/features as it is no longer be needed

--- src/features/index.js
+++ src/features/index.js
@@ -2,13 +2,11 @@
 
 const createFeature = require('./createFeature.js'),
     modifyAFeature = require('./modifyFeature.js'),
-    retrieveFeatures = require('./retrieveFeatures.js'),
 	deleteFeature = require('./deleteFeature.js'),
 	deleteAllFeatures = require('./deleteAllFeatures.js');
 
 module.exports = server => {
     server.post('/api/features', createFeature);
-	server.get('/api/features', retrieveFeatures);
 	server.del('/api/features', deleteAllFeatures);
 	server.put('/api/features/:featureId', modifyAFeature);
 	server.del('/api/features/:featureId', deleteFeature);
